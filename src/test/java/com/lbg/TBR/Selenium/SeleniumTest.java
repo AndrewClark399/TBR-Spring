b68d2f2fef2ab3ba7b3bf805e067580693a3f2ee
ZMichaelaK <korn_miku@hotmail.com>
2024-02-14 15:10:48 +0000

    cry and code boys, cry and code

--- src/test/java/com/lbg/TBR/Selenium/SeleniumTest.java
+++ src/test/java/com/lbg/TBR/Selenium/SeleniumTest.java
@@ -28,7 +28,6 @@ public class SeleniumTest {
 	@BeforeEach
 	void init() {
 		this.driver = new ChromeDriver();
-		this.driver.manage().window().maximize();
 		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	}
 
@@ -43,7 +42,10 @@ public class SeleniumTest {
 	@Order(1)
 	void testGetBuyers() {
 		this.driver.get("http://localhost:3000/Buyers");
-
+ 
+		WebElement itemAppears = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div:nth-child(1) > div > div > h4"));
+		Assertions.assertEquals("Poca Hontas", itemAppears.getText());
 	}
 
 	@Test
@@ -76,7 +78,102 @@ public class SeleniumTest {
 
 		WebElement createdBuyer = this.driver.findElement(
 				By.cssSelector("#root > header > div > div > div > div > div:nth-child(2) > div > div > h4"));
-		Assertions.assertEquals((firstName + " " + lastName), createdBuyer.getText());
-	}
 
+		WebElement createdBuyerAddress = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(2)"));
+		
+		WebElement createdBuyerPostCode = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(3)"));
+		
+		WebElement createdBuyerPhoneNumber = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(4)"));
+		
+		Assertions.assertEquals((firstName + " " + lastName), createdBuyer.getText());
+		Assertions.assertEquals((address), createdBuyerAddress.getText());
+		Assertions.assertEquals((postcode), createdBuyerPostCode.getText());
+		Assertions.assertEquals((phoneNumber), createdBuyerPhoneNumber.getText());
+
+	}
+	@Test
+	@Order(3)
+	void testGetSellers() {
+		this.driver.get("http://localhost:3000/Sellers");
+ 
+		WebElement itemAppears = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div > div > div > h4"));
+		Assertions.assertEquals("John Smith", itemAppears.getText());
+	}
+	@Test
+	@Order(4)
+	void testCreateSeller() {
+		this.driver.get("http://localhost:3000/Sellers");
+		String firstName = "Meta";
+		String lastName = "Physical";
+		String address = "4 Uncertain Potato";
+		String postcode = "C0 9CH";
+		String phoneNumber = "0758473937";
+
+		WebElement name = this.driver.findElement(By.id("fn"));
+		name.sendKeys(firstName);
+
+		WebElement name2 = this.driver.findElement(By.id("ln"));
+		name2.sendKeys(lastName);
+
+		WebElement location = this.driver.findElement(By.id("ad"));
+		location.sendKeys(address);
+
+		WebElement code = this.driver.findElement(By.id("pc"));
+		code.sendKeys(postcode);
+
+		WebElement number = this.driver.findElement(By.id("pn"));
+		number.sendKeys(phoneNumber);
+
+		WebElement addSeller = this.driver.findElement(By.cssSelector("#root > header > div > form > button"));
+		addSeller.click();
+
+		WebElement createdSeller = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div:nth-child(2) > div > div > h4"));
+								
+		WebElement createdSellerAddress = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(2)"));
+
+		WebElement createdSellerPostCode = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(3)"));
+		
+		WebElement createdSellerPhoneNumber = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div:nth-child(2) > div > div > p:nth-child(4)"));
+		
+		Assertions.assertEquals((firstName + " " + lastName), createdSeller.getText());
+		Assertions.assertEquals((address), createdSellerAddress.getText());
+		Assertions.assertEquals((postcode), createdSellerPostCode.getText());
+		Assertions.assertEquals((phoneNumber), createdSellerPhoneNumber.getText());
+	}
+	@Test
+	@Order(5)
+	
+	void testEditSelller() {
+		this.driver.get("http://localhost:3000/Sellers/Edit/1");
+		String firstName = "Bob";
+		
+		WebElement fname = this.driver.findElement(By.id("fn"));
+		fname.sendKeys(firstName);
+	    WebElement submit = this.driver.findElement(By.cssSelector("#root > header > div > form > button"));
+	    submit.click();
+	    WebElement checkName = this.driver.findElement(
+				By.cssSelector("#root > header > div > div > div > div > div > div > div > div > h4"));
+		Assertions.assertEquals((firstName), checkName.getText());
+	}
+	@Test
+	@Order(6)
+	void testDeleteSeller() {
+		 
+		this.driver.get("http://localhost:3000/Sellers");
+ 
+		WebElement deleteButton = this.driver.findElement(By
+				.cssSelector("#root > header > div > div > div > div > div > div > div > div > button.btn.btn-danger"));
+		this.driver.executeScript("arguments[0].scrollIntoView(true);", deleteButton);
+ 
+		deleteButton.click();
+ 
+	}
 }
